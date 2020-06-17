e8fc92e9b7a9acc0e20724941d27cd4224f0b1df
starsun26 <icemansbk@ya.ru>
2020-06-17 10:42:57 +0300

    fix

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,19 +1,7 @@
 // eslint-disable
 import svg4everybody from 'svg4everybody';
-import anchor from '../components/js-functions/anchor';
-import tabs from '../components/tabs/tabs';
-import dropdown from '../components/dropdown/dropdown';
-import accordion from '../components/accordion/accordion';
-import tooltips from '../components/tooltips/tooltips';
-import hamburger from '../blocks/header/header';
 
 window.onload = () => {
 	svg4everybody();
-	anchor();
-	tabs();
-	hamburger();
-	dropdown();
-	accordion();
-	tooltips();
 };
 // eslint-enable
